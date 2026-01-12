3c50e9d1c65e25fd365ecead564d912b6af62b5b
Damian Correa Borras <damcorbor@gmail.com>
2026-01-12 08:24:20 +0100

    Add files via upload

---
+++ PowerShell/practica2/ejercicio1.ps1
@@ -0,0 +1,21 @@
+﻿<# Ejercicio 1. Crea un script en lenguaje PowerShell que muestre al usuario los siguientes mensajes:
+Hola nombre de usuario
+Tu directorio de trabajo es directorio
+Perteneces al dominio Nombre_dominio
+Tu equipo se llama Nombre_equipo.#>
+
+CLEAR
+Write-Output "Hola $env:USERNAME"
+
+Write-Output ""
+Write-Output "Tu directorio de trabajo es $home"
+
+Write-Output ""
+Write-Output "Perteneces al dominio $env:USERDOMAIN"
+
+# Get-ChildItem env: | Out-String -Stream | Select-String "domain"
+
+Write-Output ""
+Write-Output "Tu equipo se llama $env:COMPUTERNAME"
+
+Write-Output ""
---
+++ PowerShell/practica2/ejercicio2.ps1
@@ -0,0 +1,11 @@
+﻿<# 
+Ejercicio2. Crea un script en PowerShell que pida dos números al usuario e imprima por pantalla su suma, la resta, la multiplicación, división y resto.
+#>
+
+$numero1 = Read-Host "Introduce el primer numero"
+$numero1 = [int]$numero1
+
+$numero2 = Read-Host "Introduce el segundo numero"
+$numero2 = [int]$numero2
+
+Write-Output "$numero1 + $numero2 = $($numero1 + $numero2)"
