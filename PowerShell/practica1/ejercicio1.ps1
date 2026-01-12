3992c575c7d960ca3368dc03961506aaf136ef23
Damian Correa Borras <damcorbor@gmail.com>
2026-01-12 08:24:05 +0100

    Add files via upload

---
+++ PowerShell/practica1/ejercicio1.ps1
@@ -0,0 +1,18 @@
+﻿clear
+$politicaEjecucion = $(Get-ExecutionPolicy)
+
+if ($politicaEjecucion -eq "RemoteSigned") {
+    Write-Output "Ya dispones de una politica adecuada, en este caso $politicaEjecucion"
+} else {
+    Write-Output "No dispones de la politica adecuada ($politicaEjecucion), aplicando politica adecuada..."
+    Set-ExecutionPolicy RemoteSigned
+    if ($? -eq "True") {
+       Write-Output "Politica aplicada correctamente... "
+    } else {
+       Write-Output "Ha habido un error..."
+       Write-Output "Abre powershell y aplica tu mismo el comando: ' Set-ExecutionPolicy RemoteSigned ' "
+    }
+}
+
+Write-Output ""
+Write-Output "Tu version actual de powershell es: $PSVersionTable"
