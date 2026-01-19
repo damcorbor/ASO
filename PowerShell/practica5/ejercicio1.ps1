7c2c1bc0f17513afa0f61cac799d9f017d84f838
Damian Correa Borras <damcorbor@gmail.com>
2026-01-19 09:16:33 +0100

    Add files via upload

---
+++ PowerShell/practica5/ejercicio1.ps1
@@ -0,0 +1,49 @@
+﻿<#
+Ejercicio 1 Crea una calculadora muy sencilla, en la que se preguntará al usuario dos números y que operación desea realizar.
+
+Ejemplo:
+
+****** CALCULADORA ********
+
+Sumar
+Restar
+Multiplicar
+Dividir
+Salir
+¿Qué desea hacer?Elige una opción:
+
+Debes crear las funciones Sumar, Restar, Multiplicar y Dividir. Cada una de estas funciones tendrá dos parámetros.
+#>
+Clear-Host
+
+
+Function Operacion ($num1, $num2) {
+    Write-Output "A continuaion se muestran las opciones disponibles:
+
+    ****** CALCULADORA ********
+
+    1: Sumar
+    2: Restar
+    3: Multiplicar
+    4: Dividir"
+
+    Write-Output ""
+
+    [int] $opcion = Read-Host "Ahora introduce la opcion que deseas elegir"
+
+    switch ($opcion) {
+        {$_ -eq 1} {
+            Write-Output "La suma es de $($num1 + $num2)"
+        } {$_ -eq 2} {
+            Write-Output "La resta es de $($num1 - $num2)"
+        } {$_ -eq 3} {
+            Write-Output "La multiplicacion es de $($num1 * $num2)"
+        } {$_ -eq 4} {
+            Write-Output "La division es de $($num1 / $num2)"
+        } default {
+            Write-Output "Valor no valido."
+        } 
+    }
+}
+
+Operacion 2 3
---
+++ PowerShell/practica5/ejercicio2.ps1
@@ -0,0 +1,14 @@
+﻿<#
+Ejercicio 2 Haciendo uso de la estructura repetitiva foreach, debes leer todos los datos del archivo usuarios.csv 
+e imprimir el nombre, apellidos y grupo del usuario
+#>
+
+Clear-Host
+
+$dir = Get-Location 
+
+$archivo = Import-Csv "usuarios.csv"
+
+foreach ($usuarios in $archivo) {
+    Write-Output "Nombre: $($usuarios.nombre), Apellido: $($usuarios.apellidos), Grupo: $($usuarios.grupo)"
+}
