83d04bfe684c3bcf4c7cb1767b9fb8c18a06c6d2
Damian Correa Borras <damcorbor@gmail.com>
2026-02-07 15:32:26 +0100

    Update asignarPerfilMovil.ps1

--- PowerShell/tema7/practica4/asignarPerfilMovil.ps1
+++ PowerShell/tema7/practica4/asignarPerfilMovil.ps1
@@ -11,11 +11,30 @@ $users = Get-ADGroupMember -Identity $grupo | Where-Object { $_.objectClass -eq 
 
 foreach ($u in $users) {
 
-    # login (carla.mateu)
+    # login (consuelo.mateu)
     $login = $u.SamAccountName
 
     # Ruta del perfil movil
     $rutaPerfil = "\\EMPRESA-DC1\Empresa_users$\$login"
+
+    # Crear carpeta del perfil movil (V6)
+    New-Item -Path "C:\Empresa_users\$login.V6" -ItemType Directory -Force
+
+    # Permisos del perfil movil
+    $acl = Get-Acl "C:\Empresa_users\$login.V6"
+    $acl.SetAccessRuleProtection($true, $false)
+
+    # usuario = control total
+    $permisoadd = @("$dom\$login", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permisoadd
+    $acl.SetAccessRule($rule)
+
+    # administradores = control total
+    $permisoadd = @("Administradores", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permisoadd
+    $acl.SetAccessRule($rule)
+
+    Set-Acl -Path "C:\Empresa_users\$login.V6" -AclObject $acl
 
     # Asignar ruta
     Set-ADUser -Identity $login -ProfilePath $rutaPerfil
